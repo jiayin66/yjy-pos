93536cbbea49f6af54e968263862ab61df70863b
jiayin <501023696@qq.com>
2019-12-25 11:11:57 +0800

    无需添加response视图器

--- java/com/yjy/controller/IndexController.java
+++ java/com/yjy/controller/IndexController.java
@@ -3,11 +3,11 @@ package com.yjy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+/*@Controller
 //@RequestMapping("/")
 public class IndexController {
 	@RequestMapping("/")
 	 public String demo() {
 	      return "index"; // 地址指向index.hml
 	  }
-}
+}*/
